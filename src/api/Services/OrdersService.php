8981e6502fbb1caced2d0e06a2817f09d619e632
vorobeyme <vorobeyme@gmail.com>
2015-12-13 07:46:06 +0200

    upd queries

--- src/api/Services/OrdersService.php
+++ src/api/Services/OrdersService.php
@@ -6,12 +6,29 @@ class OrdersService extends BaseService
 {
     public function getAll()
     {
-        return $this->db->fetchAll("SELECT `o`.*, `s`.`address` as `from_address` FROM `Order` `o` LEFT JOIN `Seller` `s` ON s.id = o.Seller_id");
+        $sql = "SELECT 
+                    `o`.*, `s`.`address` as `from_address`, 
+                    CONCAT(`c`.`first_name`, ' ', `c`.`last_name`) as `courier_name`, 
+                    `c`.`phone_number`, `h`.`Order_Status` as `order_status`
+                FROM `Order` `o` 
+                LEFT JOIN `Seller` `s` ON s.id = o.Seller_id
+                LEFT JOIN `Courier` `c` ON c.id = o.Courier_id
+                LEFT JOIN `Order_History` `h` ON h.Order_id = o.id";
+        return $this->db->fetchAll($sql);
     }
 
     public function getById($id)
     {
-        return $this->db->fetchAll("SELECT `o`.*, `s`.`address` as `from_address` FROM `Order` `o` LEFT JOIN `Seller` `s` ON s.id = o.Seller_id WHERE o.id = ?", array($id));
+        $sql = "SELECT 
+                    `o`.*, `s`.`address` as `from_address`, 
+                    CONCAT(`c`.`first_name`, ' ', `c`.`last_name`) as `courier_name`, 
+                    `c`.`phone_number`, `h`.`Order_Status` as `order_status`
+                FROM `Order` `o` 
+                LEFT JOIN `Seller` `s` ON s.id = o.Seller_id
+                LEFT JOIN `Courier` `c` ON c.id = o.Courier_id
+                LEFT JOIN `Order_History` `h` ON h.Order_id = o.id
+                WHERE o.id = ?";
+        return $this->db->fetchAll($sql, array($id));
     }
 
     function save($order)
